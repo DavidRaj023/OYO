41618707bcae045f4510714ca68ef6d0774dc7e1
David <benjamindavid023@gmail.com>
2022-03-04 16:46:00 +0530

    added insert query for owner in constant file

--- src/utils/constant.js
+++ src/utils/constant.js
@@ -1,3 +1,5 @@
 exports.ADMIN_INSERT ="INSERT INTO [dbo].[admin] (name,email,password) VALUES (@name,@email,@password)";
 exports.QUERY_GETALLDATA = "SELECT name, male, female FROM [dbo].[admin];"
 exports.QUERY_GETALLUSERNAME = "SELECT userName FROM [dbo].[admin];"
+
+exports.OWNER_INSERT ="INSERT INTO [dbo].[owner] (name,email,phoneNumber,userName,password,hotels) VALUES (@name,@email,@phoneNumber,@userName,@password,@hotels)";
