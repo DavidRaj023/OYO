15d7aa33db83a953d35da0d34444884b3789b373
David <benjamindavid023@gmail.com>
2022-03-04 16:51:06 +0530

    update new pakages

--- src/index.js
+++ src/index.js
@@ -5,7 +5,7 @@ const appRouter = require('./routes/appRoutes');
 
 const app = express();
 const port = process.env.PORT || 3000;
-
+ 
 app.use(bodyParser.urlencoded({ extended: true }));
 
 app.use(express.json());
