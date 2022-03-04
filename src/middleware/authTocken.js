cc83edd2e4a47c231ae5b4301817c6d18fd18085
David <benjamindavid023@gmail.com>
2022-03-04 16:49:06 +0530

    added new authendication token function

---
+++ src/middleware/authTocken.js
@@ -0,0 +1,16 @@
+const jwt = require('jsonwebtoken');
+const config =require('../config/authConfig');
+
+function authenticateToken (req, res, next) {
+  const token = req.headers.authtoken;
+
+  if (token == null) return res.sendStatus(401);
+
+  jwt.verify(token, config.authSecret, (err, data) => {
+    if (err) return res.status(400).send(err.message);
+    req.user = data;
+    next()
+  })
+}
+
+module.exports = authenticateToken;
